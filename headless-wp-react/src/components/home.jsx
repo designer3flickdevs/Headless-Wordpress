eb1ad8e6ede5f89f093d30a2fa92e43a11c02091
designer3flickdevs <designer3flickdevs@gmail.com>
2025-07-29 16:46:08 +0530

    update home.jsx

--- headless-wp-react/src/components/home.jsx
+++ headless-wp-react/src/components/home.jsx
@@ -37,8 +37,8 @@ function Home() {
       <h1>Blog Posts</h1>
       {posts.map(post => (
         <div key={post.id}> 
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
+          <h2>{post.title.rendered}</h2>
+          <p>{post.content.rendered}</p>
         </div>
       ))}
     </div>
