0001d489faf777360fc9d3a3804cf7a3e050383b
designer3flickdevs <designer3flickdevs@gmail.com>
2025-07-30 12:45:28 +0530

    update

--- headless-wp-react/src/App.jsx
+++ headless-wp-react/src/App.jsx
@@ -1,20 +1,8 @@
 import Home from './components/home'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
   return (
-    // <Home />
-    <div>
-      <h1>fdgdgdf</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      
+    <Home />
   )
 }
 
