cc87cf35c9ce5234bbac22b650a2d8d8df6f16a0
designer3flickdevs <designer3flickdevs@gmail.com>
2025-07-30 12:40:26 +0530

    update

--- headless-wp-react/src/components/home.jsx
+++ headless-wp-react/src/components/home.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 function Home() {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -36,6 +37,14 @@ function Home() {
 
   return (
     <div>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
       <h1>Blog Posts</h1>
       {posts.map(post => (
         <div key={post.id}> 
