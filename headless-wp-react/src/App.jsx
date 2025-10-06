9e0526ec7ede8fff7192bd54eae24f4249194286
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-06 10:17:52 +0530

    update

--- headless-wp-react/src/App.jsx
+++ headless-wp-react/src/App.jsx
@@ -1,6 +1,6 @@
 // import Posts from './components/pages/Posts'
 // import Test from './components/test'
-import Header from "./components/pages/"
+import Header from './components/pages/Header'
 
 function App() {
   return (
