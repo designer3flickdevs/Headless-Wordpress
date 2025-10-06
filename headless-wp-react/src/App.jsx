de59db3e973978263599d270b75cd0ad1bc758fb
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-06 10:06:36 +0530

    updare header

--- headless-wp-react/src/App.jsx
+++ headless-wp-react/src/App.jsx
@@ -1,10 +1,12 @@
 // import Posts from './components/pages/Posts'
-import Test from './components/test'
+// import Test from './components/test'
+import Header from "./components/pages/Header"
 
 function App() {
   return (
     // <Posts />
-    <Test />
+    // <Test />
+    <Header />
   )
 }
 
