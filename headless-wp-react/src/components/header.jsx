9862ae45710e3baeeec764f8e72ff21030b14615
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-08 16:27:01 +0530

    update

--- headless-wp-react/src/components/header.jsx
+++ headless-wp-react/src/components/header.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Sun } from "lucide-react";
 import logo from "../assets/logo.webp";
+import "../App.css";
 
 const Header = () => {
   const [menuItems, setMenuItems] = useState([]);
