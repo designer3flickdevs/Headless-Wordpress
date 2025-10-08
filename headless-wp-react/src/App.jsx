212e372dd0bea41b487a690660710f1fd6c113d3
designer3flickdevs <designer3flickdevs@gmail.com>
2025-10-08 11:07:05 +0530

    update

--- headless-wp-react/src/App.jsx
+++ headless-wp-react/src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home/home";
+import Test from "./components/pages/Test/test";
 
 function App() {
   return (
-    <Home />
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/test" element={<Test />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ headless-wp-react/src/components/pages/Test/test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../../../App.css";
+import Header from "../Header.jsx";
+
+const Test = () => {
+  return (
+    <>
+      <Header />
+      <section className="test-section">
+        <h2>This is the Test Page</h2>
+        <p>Welcome to the test page. You can add your content here.</p>
+      </section>
+    </>
+  );
+};
+
+export default Test;
